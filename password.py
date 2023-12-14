50971a51e0654187a460f63aeb397a9d5a3d95d5
uvauvuv <uvarani2401@gmail.com>
2023-12-14 18:12:16 +0530

    Passwordcheck

---
+++ password.py
@@ -0,0 +1,17 @@
+'''
+Q.Write a program to set password and verify if the user is logged in with correct password.
+'''
+
+PASSWORD = "Pythonuser"
+#task how to get input from user   enter pass
+#verify and matched - " welcome uva" 
+# if not password incorrect. - 
+
+getinput = input ("Password please : \n")
+if getinput == PASSWORD:
+    print ("Welcome Uva")
+else :
+    print ("Password incorect try again !")
+    
+
+
