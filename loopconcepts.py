a389d41aabb1f3a86e112727407fca4c7fac0290
uvauvuv <uvarani2401@gmail.com>
2023-12-20 03:35:19 +0530

    loops

---
+++ loopconcepts.py
@@ -0,0 +1,15 @@
+#Two loops we have for and while
+
+
+listofchocolates = ["Bounty", "Dairy Milk","Fivestar","KitKat","Munch","Milky bar","Eclairs","Ferrore"]
+
+listofjuiceflavour  = ["Mango","Pineapple","apple","Orange","Kiwi","Leechi","Avacado"]
+
+#for i in listofchocolates:
+   # print (i)
+
+for i in listofjuiceflavour[0:5]:
+
+    if (i.lower()  in  ["leechi" ,"orange"]): 
+        print ("My favourite juice  is :" , i)
+   
