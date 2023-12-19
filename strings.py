842608896f9a315648e1e45ddfe05391f00aecc3
uvauvuv <uvarani2401@gmail.com>
2023-12-19 19:40:10 +0530

    Strings operation

--- strings.py
+++ strings.py
@@ -1,10 +1,6 @@
          # 01234567891011 10 
 string = "Hello World!"
-                 # -2-1  -2
-stringnew = "My name is YuvaraniBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBZ"
-
-'''
-print(string+ " " +stringnew+" My husband's name is Barath mama" )#Concat add two strings
+                 # -2-1  
 #space we can give in the strings itself
 num = 2401
 print (num*2)
@@ -12,11 +8,11 @@ print (num*2)
 
 #slicing 
 #print (string[:])
-print (stringnew [-4:]) #slicing
+#print (stringnew [-4:]) #slicing
 print (string[-1]) #indexing
 print (string [:-1]) #slicing 
 print (string [:11])
-'''
+
 
 s = "#C*H$E@N!N^A&I)"
 #range(start,end,step)\\
