197f2c88d1363980d1c5a73da48d4063d56ebaf9
uvauvuv <uvarani2401@gmail.com>
2023-12-19 19:35:37 +0530

    string operations

---
+++ strings.py
@@ -0,0 +1,33 @@
+         # 01234567891011 10 
+string = "Hello World!"
+                 # -2-1  -2
+stringnew = "My name is YuvaraniBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBZ"
+
+'''
+print(string+ " " +stringnew+" My husband's name is Barath mama" )#Concat add two strings
+#space we can give in the strings itself
+num = 2401
+print (num*2)
+#4802
+
+#slicing 
+#print (string[:])
+print (stringnew [-4:]) #slicing
+print (string[-1]) #indexing
+print (string [:-1]) #slicing 
+print (string [:11])
+'''
+
+s = "#C*H$E@N!N^A&I)"
+#range(start,end,step)\\
+#print (s[-2::-2])
+
+message = "close the session"
+
+print (message.upper())
+print (message.lower())
+print (message.startswith("cl"))
+print (message.replace("close","open"))
+print (message.index("s"))
+
+ 
