9f3596b605480a4723464df9675365c5551cb814
uvauvuv <uvarani2401@gmail.com>
2023-12-21 16:12:28 +0530

    listmethods

--- loopconcepts.py
+++ loopconcepts.py
@@ -4,13 +4,25 @@
 listofchocolates = ["Bounty", "Dairy Milk","Fivestar","KitKat","Munch","Milky bar","Eclairs","Ferrore"]
 buychocolateslist= []
 
+'''
 buychocolateslist.append("Kitkat")
 buychocolateslist.append("Bounty")
 listofchocolates.remove("KitKat")
 listofchocolates.remove("Bounty")
 print  (buychocolateslist)
 print (listofchocolates)
- 
+'''
+
+for i in listofchocolates:
+    if i.lower() in ["bounty" ,"kitkat"]:
+        #Either i = Bounty or KitKat
+        buychocolateslist.append(i)
+        listofchocolates.remove (i)
+
+        print  (buychocolateslist)
+        print (listofchocolates)
+
+
 
 #Kitkat and bounty got over , in empty these two chocs need to move 
 #listofchocolates -  should remove this
@@ -24,5 +36,6 @@ listofjuiceflavour  = ["Mango","Pineapple","apple","Orange","Kiwi","Leechi","Ava
 #for i in listofjuiceflavour[0:5]:
 
     #if (i.lower()  in  ["leechi" ,"orange"]): 
+    #
        # print ("My favourite juice  is :" , i)
    
