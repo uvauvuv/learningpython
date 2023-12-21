a11682b5c8b63aeb2012446a9a5deff57df10e77
uvauvuv <uvarani2401@gmail.com>
2023-12-21 17:10:31 +0530

    listprograms

--- listprograms.py
+++ listprograms.py
@@ -5,10 +5,13 @@
 listofchocolates = [" Bounty " , " Dairy Milk ","Fivestar","KitKat","Mumch","Milky bar","Eclairs","Ferrore"]
 
 listofjuiceflavour  = ["Mango","Pineapple","apple","Orange","Kiwi","Leechi","Avacado"]
+listofchocolates.remove("Ferrore")
+listofchocolates.append("Uva")
+
 
 both = listofchocolates+listofjuiceflavour
 joinvar = "**".join(both)
-splitvar = joinvar.split("*")
+splitvar = joinvar.split("**")
 #print ("@".join(listofchocolates+listofjuiceflavour))
 print (both)
 print (joinvar)
