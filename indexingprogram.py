f8a4a8d9b2e6d9fe29b64a2891ef57af59758087
uvauvuv <uvarani2401@gmail.com>
2023-12-21 17:12:48 +0530

    index

---
+++ indexingprogram.py
@@ -0,0 +1,8 @@
+datas = ["Yuva", 55, True, "Barath"]
+value  = list(("Yuva", "Python"))
+
+print (datas, value)
+#print (value)
+print ( "Index position of Yuva is ", datas.index("Yuva"))
+print (len(datas))
+print (type(datas), type (datas))
