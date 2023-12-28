38d32199a7520e466ad620b432f049adbe7a4ab2
uvauvuv <uvarani2401@gmail.com>
2023-12-28 15:36:14 +0530

    bakery

---
+++ homeworkpograms/bakery.py
@@ -0,0 +1,31 @@
+#Bakery company (title , founders), 2 locations(Chennai, Bangalore), Each location 4 employees (name, age, empid, task(sweet,bun,puff)
+#at a time in a bakery in chennai location what items are there )
+#eachlocation (itemsavailable) count
+
+check = 4 # () if statement (please add more buns, sweet ) in that paticular locations 
+#Task2: we can tell this in task  line 5
+
+fooddepartment = dict()
+bakery = {{"Title" :"Uva bakery"},"Founders" :["Yuva","Bharath"]}
+locations = ["Chennai" ,"Bangalore"]
+fooddepartment ["Bakery","locations"] = bakery ,locations 
+itemsavailable = ["sweets","puffs," "biscuits","cakes" ,"toast" ," icecream" ,"jampackets","chocolates"]
+
+ 
+employee1 ={"Name" : "Srivenkatesh" ,"Age" :29,"Task" : "sweetsmaking","location" : "Chennai"}
+employee2 = {"Name" : "Karur" ,"Age" :29,"Task" : "puffmaking" , "location" : "Chennai"}
+employee3 ={"Name" : "Pride" ,"Age" :30,"Task" : "biscuitscooking","location" : "Chennai"}
+employee4 = {"Name" : "Views" ,"Age" :28,"Task" : "cakebaking" , "location" : "Chennai"}
+locations[0] =[employee1,employee2,employee3,employee4]
+print (locations[0])
+
+employee5 ={"Name" : "Nasha" ,"Age" :29,"Task" : "toastmaking","location" : "Bangalore"}
+employee6 = {"Name" : "srigupta" ,"Age" :29,"Task" : "chocolatesmaking" , "location" : "Bangalore"}
+employee7 ={"Name" : "bhavan" ,"Age" :30,"Task" : "icecreambox","location" : "Bangalore"}
+employee8 = {"Name" : "vb" ,"Age" :28,"Task" : "jampackets" , "location" : "Bangalore"}
+locations[1] =[employee5,employee6,employee7,employee8]
+print (locations[1])
+print (bakery["Title"])
+
+if i
+
